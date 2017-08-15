ad2ddd41fd9df4a3eae50949061a8a8fc23c6900
Thomas Couillard <thomas@thomascouillard.ca>
2017-08-15 03:20:59 -0500

    fixed issue with googleCSE key and refactored node env vars

--- googleConfig.js
+++
@@ -1,6 +0,0 @@
-let googleConfig = {
-    CSE_ID: process.env.GOOGLE_CSE_ID,
-    API_KEY: process.env.GOOGLE_API_KEY
-};
-
-module.exports = googleConfig;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
+
 const fs = require('fs'),
 		path = require('path'),
 		Twit = require('twit'),
 		config = require(path.join(__dirname, 'config.js')),
 		randomWord = require("random-word"),
 		tinyText = require("tiny-text"),
-		request = require("request"),
-		googleConfig = require(path.join(__dirname, 'googleConfig.js'));
+		request = require("request");
 
 // googleConfig.js and config.js required as per google-images / twit npm
 // packages - replace process.env.____ with your keys
@@ -14,7 +15,7 @@ const fs = require('fs'),
 //GOOGLE API FOR PULLING IMAGES
 //
 const GoogleImages = require("google-images");
-const client = new GoogleImages(googleConfig.CSE_ID, googleConfig.API_KEY);
+const client = new GoogleImages(process.env.GOOGLE_CSE_ID, process.env.GOOGLE_API_KEY);
 const imageSearch = (queryString, callback) => {
 		client
 				.search(queryString)
@@ -91,34 +92,3 @@ imageSearch(text, output => {
 		}, 10000);
 
 });
-
-// //random tweet interval between 1-7 hours
-// const interval = () => {
-// 		const min = 3600000,
-// 				max = 21600000;
-
-// 		return Math.floor(Math.random() * (max - min)) + min;
-// };
-// //
-// //automation of tweeting process, making the app into a bot
-// //
-// setInterval(() => {
-// 		const text = randomWord();
-		
-// 		console.log(interval());
-// 		imageSearch(text, output => {
-// 				//callback to run in sync
-// 				console.log(`Text: ${text}`);
-// 				//regex to scrub query strings and url paths keeping only filenames.
-// 				const pat = /[^/\\&\?]+\.\w{3,4}(?=([\?&].*$|$))/;
-// 				let res = pat.exec(output);
-// 				request(output).pipe(fs.createWriteStream(`./images/${res[0]}`));
-// 				// timeout required otherwise Twit produces Error status 400 Unrecognized Media
-// 				// Format due to file not being saved. Working on fix to run sync and avoid
-// 				// using timeouts entirely.
-// 				setTimeout(() => {
-// 						postTweet(res[0], text);
-// 				}, 10000);
-
-// 		});
-// }, interval);
